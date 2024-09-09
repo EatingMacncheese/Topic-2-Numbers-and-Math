0c8e834d93f18c49054a59c8b182c63be9d47794
Tiago Teague Violante <339784910@GoTVDSB.ca>
2024-09-09 08:26:45 -0400

    Add project files.

---
+++ Topic-2 Numbers and Math/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Topic_2_Numbers_and_Math
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("six" + 6);
+            Console.WriteLine("Hens " + (10 + 5 * 2));
+            Console.WriteLine("
+        }
+    }
+}
